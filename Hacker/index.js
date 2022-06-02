61a119b4ba51c96cbdd9f279e3e76879dc2c3de4
Bhoopesh <82646284+bhoopesh369@users.noreply.github.com>
2022-06-02 11:29:27 +0530

    Add files via upload

---
+++ Hacker/index.js
@@ -0,0 +1,191 @@
+
+
+var level = 1;
+var score = 0;
+var Max = 0;
+
+var MaxScore;
+MaxScore = window.localStorage.getItem("MaxScore");
+document.querySelector(".MaxScore").innerHTML = "Max Score: " + MaxScore;
+
+
+var gamePattern = [];
+var playerPattern = [];
+var inGame = false;
+
+var mins = 0 ;
+var secs ; 
+
+var myInterval;
+var intId;
+
+function timeer(val){
+    function updateTime(){
+        
+        const min = Math.floor(secs/60);
+        let sec = secs % 60;
+        secs--;
+        if(secs<0){
+            timeleft.style.color = "yellow";
+            timeleft.innerHTML = "00:00";
+            GameisOver();  
+            restart();
+        } 
+        if(sec>=10){
+          timeleft.innerHTML =  '0'+min + ':' + sec;
+        }
+        else{
+          timeleft.innerHTML =  '0'+min + ':0' + sec;
+        }
+    }
+
+    timeleft = document.getElementById("timer");
+
+    myInterval = setInterval(updateTime,1000);
+
+}    
+
+function clearTimer(){
+    clearInterval(myInterval);
+}
+    
+function playSound(){
+    var aud = new Audio("sounds/scifi.mp3");
+    aud.play();
+}
+
+function playSound2(){
+    var aud = new Audio("sounds/juxb.mp3");
+    aud.play();
+}
+
+function gameOverSound(){
+    var aud = new Audio("sounds/wrong.mp3");
+    aud.play();
+}
+
+
+
+function tileEffects(tile){
+    var activeTile = document.getElementById(tile);
+    activeTile.classList.add("pressed");
+    setTimeout(function(){
+        activeTile.classList.remove('pressed');
+    },300);
+}
+
+function previousTileEffects(){
+    var t=0;
+    document.getElementById("timer").innerHTML= '00:00';
+    level++;
+    document.querySelector("h2").innerHTML = "Level " + level;
+    function effects(){
+      var activeTile = document.getElementById(gamePattern[t]);
+      playSound2();
+      activeTile.classList.add("pressed");
+      setTimeout(function(){
+        activeTile.classList.remove('pressed');
+      },250);
+      t++;
+      if(t>= gamePattern.length){
+
+          clearPreviosTileEffects();
+      }
+    }
+    intId = setInterval(effects,1000);  
+    
+}
+function clearPreviosTileEffects(){
+    clearInterval(intId);
+}
+
+function nextTile(){
+    //clearPreviosTileEffects();
+    clearTimer();
+    MaxScore = window.localStorage.getItem("MaxScore");
+    document.querySelector(".MaxScore").innerHTML = "Max Score: " + MaxScore;
+
+    playerPattern=[];
+    var rand = Math.floor(Math.random() *36) + 1;
+    gamePattern.push(''+rand);
+    playSound2();
+    tileEffects(rand);
+    
+    secs = 5*(gamePattern.length - 1) + 10;
+    timeer();
+}
+
+function GameisOver(){
+    //clearPreviosTileEffects();
+    document.querySelector("body").classList.add("game-over");
+      document.querySelector("h2").innerHTML = "Press Any Key to Restart" ;
+      clearTimer();
+      gameOverSound();
+      setTimeout(function () {
+        alert("Game Over");
+      }, 230);
+
+}
+
+function restart(){
+    Max = window.localStorage.getItem("MaxScore");
+    if(score>Max){
+        window.localStorage.setItem("MaxScore" , score);
+    }
+    document.querySelector(".MaxScore").innerHTML = "Max Score: " + Max;
+    playerPattern=[];
+    gamePattern=[];
+    level=1;
+    inGame = false;
+}
+
+document.addEventListener('keypress',function(){
+    if (!inGame) {
+        playerPattern=[];
+        gamePattern=[];
+        document.querySelector("body").classList.remove("game-over");
+        var levelhtml = "Level " + level;
+        score = 0;
+        document.getElementById("timer").innerHTML= '00:00';
+        document.querySelector("h3").innerHTML = "Score : " + score ;
+        document.querySelector("h2").innerHTML = levelhtml;
+        nextTile();
+        inGame = true;
+        
+    }
+  });
+
+ function handleClick(itr) {
+
+    if (gamePattern[itr] === playerPattern[itr]) {
+      score = score + secs +1;  
+      document.querySelector("h3").innerHTML = "Score : " + score ;
+      if (playerPattern.length === gamePattern.length){  
+        clearTimer();
+        previousTileEffects();
+        setTimeout(function () {
+            nextTile();
+        }, (gamePattern.length + 1)*1000);
+      }
+    } 
+    else {
+      GameisOver();  
+      restart();
+    }
+}
+
+
+for(var n=0;n<36;n++){
+    document.querySelectorAll(".btn")[n].addEventListener("click", gameMain); 
+}
+
+function gameMain(){
+    var userChosenTile = this.id;
+    playerPattern.push(userChosenTile);
+  
+    playSound();
+    //tileEffects(userChosenTile);
+    handleClick(playerPattern.length-1);
+ }
+
+
---
+++ Normal/index.js
@@ -0,0 +1,145 @@
+
+var started = true;
+var level = 0;
+
+var gamePattern = [];
+var playerPattern = [];
+
+function playSound(){
+    var aud = new Audio("sounds/scifi.mp3");
+    aud.play();
+}
+
+function gameOverSound(){
+    var aud = new Audio("sounds/wrong.mp3");
+    aud.play();
+}
+
+function playSound2(){
+    var aud = new Audio("sounds/scifi2.mp3");
+    aud.play();
+}
+
+function tileEffects(tile){
+    var activeTile = document.getElementById(tile);
+    activeTile.classList.add("pressed");
+    setTimeout(function(){
+        activeTile.classList.remove('pressed');
+    },200);
+}
+function nextTile(){
+    var rand = Math.floor(Math.random() *16) + 1;
+    gamePattern.push(''+rand);
+    tileEffects(rand);
+    playSound2();
+}
+
+var intId;
+function previousTileEffects(){
+    var t=0;
+    //document.getElementById("timer").innerHTML= '00:00';
+    //level++;
+    document.querySelector("h2").innerHTML = "Level " + level;
+    function effects(){
+      var activeTile = document.getElementById(gamePattern[t]);
+      playSound2();
+      activeTile.classList.add("pressed");
+      setTimeout(function(){
+        activeTile.classList.remove('pressed');
+      },250);
+      t++;
+      if(t>= gamePattern.length){
+
+          clearPreviosTileEffects();
+      }
+    }
+    intId = setInterval(effects,1000);  
+    
+}
+function clearPreviosTileEffects(){
+    clearInterval(intId);
+}
+
+document.addEventListener('keypress',function(){
+    if(started == true){
+       started = false;
+       nextTile();
+    }
+    
+
+    function handleClick(){
+       var userChosenTile = this.id;
+       playerPattern.push(userChosenTile);
+       //tileEffects(userChosenTile);
+       playSound();
+       var temp=[];
+       for(let z=0 ; z<gamePattern.length;z++){
+           temp.push(gamePattern[z]);
+       }
+       console.log(temp);
+       console.log(playerPattern);
+       console.log(gamePattern);
+       
+       var gameProgress = 0;
+
+       for(i=0;i<playerPattern.length;i++){
+
+        if(temp.includes(playerPattern[i])){   
+            //temp.pop(playerPattern[i]);
+            const index = temp.indexOf(playerPattern[i]);
+            if (index > -1) {
+                temp.splice(index, 1); 
+            }
+            console.log(temp);
+            gameProgress+=1 ;
+        }
+        else {
+            gameOverSound();
+            document.querySelector("body").classList.add("game-over");
+            var z = 1;
+            if(level==0){
+                z = 0;
+            }
+            document.querySelector("h2").innerHTML = "your score is : " + 10* Math.pow(2,level) * z;
+            setTimeout(GameOver,250);
+          }
+      
+       }
+
+
+       function GameOver(){ 
+        alert("Game Over");
+        location.reload(true); 
+      }
+
+
+       var Temp2 = [], Temp3 = [];
+       
+       for(var g of gamePattern){
+           Temp2.push(g);
+       }
+       for(var p of gamePattern){
+           Temp3.push(p);
+       }
+
+
+       Temp2.sort();
+       Temp3.sort();
+    
+       if(JSON.stringify(Temp3) === JSON.stringify(Temp2) && gameProgress == Temp2.length){
+           level++;
+           var levelhtml = "Level " + level;
+           document.querySelector("h2").innerHTML = levelhtml;
+           previousTileEffects();
+           setTimeout(nextTile, (gamePattern.length + 1)*1000);
+           playerPattern=[];
+       }
+
+    }
+
+    for(var n=0;n<16;n++){
+        document.querySelectorAll(".btn")[n].addEventListener("click",handleClick);  
+       }
+
+});
+
